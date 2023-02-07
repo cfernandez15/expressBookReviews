305170b8d83d3bb3e7df32f62059d94a0e106a81
Cristian Fernandez <criistianyqc@gmail.com>
2023-02-07 11:00:38 -0500

    Small changes
    Signed-off-by: Cristian Fernandez <criistianyqc@gmail.com>

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,7 +4,6 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
   //Write your code here
   const username = req.body.username;
@@ -23,29 +22,25 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(200).send(JSON.stringify(books, null, 4));
+  getAllBooks
+  .then( result => res.status(200).send(result))
+  .catch( error => res.status(400).send('There was an error '+error))
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  let isbn = req.params.isbn;
-  let book = books[isbn];
-  return res.status(200).send(book);
+  getBookByISBN(req.params.isbn)
+  .then ( result => res.send(result))
+  .catch ( error => res.status(400).send(error))
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  let author = req.params.author;
-  let keys = Object.keys(books);
-  let filtered_books = [];
-  keys.forEach( (key) => {
-    if (books[key].author === author) {
-        filtered_books.push(books[key]);
-    }
-  })
-  return res.status(200).send(filtered_books);
+  getBooksByAuthor(req.params.author)
+  .then( result => res.send(result))
+  .catch( error => res.status(400).send(error))
 });
 
 // Get all books based on title
@@ -68,5 +63,48 @@ public_users.get('/review/:isbn',function (req, res) {
   let reviews = books[isbn].reviews;
   return res.status(200).send(reviews);
 });
+
+let getAllBooks = new Promise((resolve, reject) => {
+    resolve(books);
+})
+
+let getBookByISBN = (isbn) => {
+    return new Promise((resolve, reject) => {
+        if (books[isbn]) {
+            resolve(books[isbn]);
+        } else {
+            reject('Book no found')
+        }
+    })
+}
+
+let getBooksByAuthor = (author) => {
+    return new Promise((resolve, reject) => {
+        let keys = Object.keys(books);
+        let filtered_books = [];
+        keys.forEach( (key) => {
+          if (books[key].author === author) {
+              filtered_books.push(books[key]);
+          }
+        })
+        if (filtered_books > 0) {
+            resolve(filtered_books);
+        } else {
+            reject('Book not found')
+        }
+    })
+}
+
+let getBookByTitle = (title) => {
+    return new Promise((resolve, reject) => {
+        let keys = Object.keys(books);
+        keys.forEach( (key) => {
+        if (books[key].title === title) {
+            resolve(books[key]);
+            }
+        })
+        reject('Book not found');
+    })
+}
 
 module.exports.general = public_users;
